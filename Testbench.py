5c61198e408592feeb9ee23508418f59251c2fe0
Sebastian Wehmeier <sebastian.wehmeier@gmail.com>
2026-01-19 14:07:55 +0100

    improve plot

--- Testbench.py
+++ Testbench.py
@@ -1,6 +1,7 @@
 from IRK import IRK, Model
 import casadi as ca
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import numpy as np
 import time
 
@@ -47,7 +48,10 @@ def TestBench(P1:Model, P2:Model, numOfsteps:int, dt: float, startCondition1, st
 
 
     x_plot = np.array(T_plot, dtype=float)
-    plt.figure( figsize = (20,10))
+    plt.figure( figsize = (14,9))
+    
+
+
 
     plt.subplot(2,4,1)
     plt.plot(x_plot,Y_Plot1)
@@ -61,24 +65,37 @@ def TestBench(P1:Model, P2:Model, numOfsteps:int, dt: float, startCondition1, st
     plt.plot(x_plot,Y_Plot3)
     plt.title("difference")
 
-    plt.subplot(2,4,4)
-    plt.plot(x_plot[1:], needed_iterations_plot1)
-    plt.title("iterrations needed (Newton)")
+    ax = plt.subplot(2,4,4)
+    plt.plot(x_plot[1:], needed_iterations_plot1, label= 'Newton')
+    plt.plot(x_plot[1:], needed_iterations_plot2, label = "Halley")
+    plt.legend(bbox_to_anchor=(1, 1))
+    plt.ylim(0,3)
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    plt.title("iterrations needed ")
 
+
+    textstr = f"on average {(sum(executionTimePlot1)/(1e6*len(executionTimePlot1))):2f}"
+    props = dict(boxstyle='round', facecolor='white', alpha=0.5)
+    
     plt.subplot(2,4,5)
-    plt.plot(x_plot[1:], needed_iterations_plot2)
-    plt.title("iterrations needed (Halleys)")
-
-    plt.subplot(2,4,6)
     plt.plot(x_plot[1:], executionTimePlot1)
+    plt.gca().text(0.03, 0.95, textstr, transform=plt.gca().transAxes,
+                    fontsize=12, verticalalignment='top', bbox=props)
     plt.title("execution time in ns (Newton)")
 
-    plt.subplot(2,4,7)
+    plt.subplot(2,4,6)
+
     plt.plot(x_plot[1:], executionTimePlot2)
+    textstr = f"on average {(sum(executionTimePlot2)/(1e6*len(executionTimePlot2))):2f}"
+    plt.gca().text(0.03, 0.95, textstr, transform=plt.gca().transAxes,
+                    fontsize=12, verticalalignment='top', bbox=props)
     plt.title("execution time in ns (Halleys)")
 
-    plt.show()
+    
+    
 
+
+    plt.show()
 
 if __name__ == "__main__":
 
@@ -101,4 +118,4 @@ if __name__ == "__main__":
     startCondition1 = ca.DM(1)
     startCondition2 = ca.DM([1,0.1353352832366127,0.01831563888873418]) # w1 = e^-2*1 = 0.13533... ;  w2 = e^-4*1 = 0.01831... ;
     TestBench(P1,P2,numOfsteps, dt, startCondition1, startCondition2)
-
+    
