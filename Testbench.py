a6eb1689e92e0177343052061497780388cef328
Sebastian Wehmeier <sebastian.wehmeier@gmail.com>
2026-01-26 12:56:53 +0100

    testbench

--- Testbench.py
+++ Testbench.py
@@ -56,7 +56,7 @@ def TestBench(P1:Model, P2:Model, numOfsteps:int, dt: float, startCondition1, st
     plt.plot(x_plot[1:], executionTimePlot1)
     plt.gca().text(0.03, 0.95, textstr, transform=plt.gca().transAxes,
                     fontsize=12, verticalalignment='top', bbox=props)
-    plt.title("execution time in ns (Newton)")
+    plt.title("execution time  (Newton)")
 
     plt.subplot(2,4,6)
 
@@ -64,23 +64,25 @@ def TestBench(P1:Model, P2:Model, numOfsteps:int, dt: float, startCondition1, st
     textstr = f"on average {(sum(executionTimePlot2)/(1e6*len(executionTimePlot2))):2f}"
     plt.gca().text(0.03, 0.95, textstr, transform=plt.gca().transAxes,
                     fontsize=12, verticalalignment='top', bbox=props)
-    plt.title("execution time in ns (Halleys)")
+    plt.title("execution time  (Halleys)")
 
     plt.show()
 
 
-def TestBench2(P1:Model, P2:Model, timeToSimulate:int, dt: float, startCondition1, startCondition2):
+def TestBench2(P1:Model, P2:Model,P3:Model ,timeToSimulate:int, dt: float, startCondition1, startCondition2):
     RK1 = IRK(P1,"Newton", dt,10000,1e-6,stages=2)
     RK2 = IRK(P2, "Halleys",dt, 10000,1e-6, stages=2)
-
+    RK3 = IRK(P3, "Newton",dt,10000,1e-6,stages=2)
 
         
     # simulate
     F_plot1, T_plot, needed_iterations_plot1, executionTimePlot1 = simulateFor(RK1,int(timeToSimulate/dt),dt,startCondition1)
     F_plot2, _, needed_iterations_plot2, executionTimePlot2 = simulateFor(RK2,int(timeToSimulate/dt),dt,startCondition2)
+    F_plot3, _, needed_iterations_plot3, executionTimePlot3 = simulateFor(RK3,int(timeToSimulate/dt),dt,startCondition2)
+
     #plot answer
     originalGraph = np.array( [float(F_plot1[i]) for i in range(len(F_plot1))])
-    difference = np.array([abs(float(F_plot1[i]-F_plot2[i][0])) for i in range(len(F_plot2))])
+    difference = np.array([abs(float(F_plot1[i]-F_plot3[i][0])) for i in range(len(F_plot2))])
 
     errorPlot1 = []
     meanTimePlot1 = []
@@ -89,39 +91,63 @@ def TestBench2(P1:Model, P2:Model, timeToSimulate:int, dt: float, startCondition
     errorPlot2 = []
     meanTimePlot2 = []
     averageIterationPlot2 = []
+    
+
+    errorPlot3 = []
+    meanTimePlot3 = []
+    averageIterationPlot3 = []
 
     deltaT = []
-    for i in range(2,10):#
+    for i in range(3,10):#
         numberOfSteps = 2**i
         print(numberOfSteps)
         RK1.dt = timeToSimulate/numberOfSteps
         RK2.dt = timeToSimulate/numberOfSteps
+        RK3.dt = timeToSimulate/numberOfSteps
+
         deltaT+=[RK1.dt]
 
         F_plotTmp1, _, iterAverage1, exeTimeMean1 = simulateForReturnMeanTime(RK1, numberOfSteps,RK1.dt,startCondition1)  
         F_plotTmp2, _, iterAverage2, exeTimeMean2 = simulateForReturnMeanTime(RK2, numberOfSteps,RK2.dt,startCondition2)  
+        F_plotTmp3, _, iterAverage3, exeTimeMean3 = simulateForReturnMeanTime(RK3, numberOfSteps,RK3.dt,startCondition2)  
+
 
         errorPlot1 +=[float(abs(F_plot1[-1]-(F_plotTmp1[-1])))]
         errorPlot2 +=[float(abs(F_plot1[-1]-F_plotTmp2[-1][0]))]
+        errorPlot3 +=[float(abs(F_plot1[-1]-F_plotTmp3[-1][0]))]
+
+
 
         meanTimePlot1 +=[exeTimeMean1]
         meanTimePlot2 +=[exeTimeMean2]
+        meanTimePlot3 +=[exeTimeMean3]
+
 
         averageIterationPlot1 += [iterAverage1]
         averageIterationPlot2 += [iterAverage2]
+        averageIterationPlot3 += [iterAverage3]
+
     T_plot,originalGraph,difference,
     deltaT.reverse()
     errorPlot1.reverse()
     errorPlot2.reverse()
+    errorPlot3.reverse()
+
     averageIterationPlot1.reverse()
     averageIterationPlot2.reverse()
+    averageIterationPlot3.reverse()
+
     meanTimePlot1.reverse()
     meanTimePlot2.reverse()
+    meanTimePlot3.reverse()
+
     
     pyPlot(T_plot, originalGraph, "Time", "Value", "orginal Graph")
     pyPlot(T_plot, difference, "Time", "Value", "differnce between original System and ")
     pyPlot(deltaT, errorPlot1, "delta T", "error", "differnce depending on the step size (Newton)")
     pyPlot(deltaT, errorPlot2, "delta T", "error", "differnce depending on the step size (Halleys)")
+    pyPlot(deltaT, errorPlot3, "delta T", "error", "differnce depending on the step size (Newton)")
+
 
 
 
@@ -144,14 +170,14 @@ def simulateFor(ImRK: IRK,numOfSteps:int, dt:float ,startcondition)-> tuple[list
     needed_iterations_plot =  []
     T_plot = [0]
 
-    for i in range(1,numOfSteps):
+    for i in range(0,numOfSteps):
         starttime = time.perf_counter_ns()
-        answer = ImRK.solve(F_plot[i-1])
+        answer = ImRK.solve(F_plot[i])
         executionTimePlot +=[time.perf_counter_ns()-starttime]
         F_plot += [answer]
         needed_iterations_plot +=[ImRK.lastIterationCount]
        
-        T_plot += [i*dt ]
+        T_plot += [(i+1)*dt ]
 
     return F_plot, np.array(T_plot, dtype=float), needed_iterations_plot,executionTimePlot
 def simulateForReturnMeanTime(ImRK: IRK,numOfSteps:int, dt:float ,startcondition)-> tuple[list,list,float,float]:
@@ -160,14 +186,14 @@ def simulateForReturnMeanTime(ImRK: IRK,numOfSteps:int, dt:float ,startcondition
     needed_iterations_average =  0
     T_plot = [0]
 
-    for i in range(1,numOfSteps):
+    for i in range(0,numOfSteps):
         starttime = time.perf_counter_ns()
-        answer = ImRK.solve(F_plot[i-1])
+        answer = ImRK.solve(F_plot[i])
         executionTimePlot.add(time.perf_counter_ns()-starttime)
         F_plot += [answer]
         needed_iterations_average +=ImRK.lastIterationCount
        
-        T_plot += [i*dt ]
+        T_plot += [(i+1)*dt ]
 
     return F_plot, np.array(T_plot, dtype=float),needed_iterations_average/numOfSteps, executionTimePlot[len(executionTimePlot)//2]
     
@@ -182,21 +208,29 @@ if __name__ == "__main__":
     P1.nx = 1
     P1.x = ca.SX.sym("x", P1.nx)
     P1.model = ca.exp(-2*P1.x)+ ca.exp(-4*P1.x)
-    # create polynomialized version of the system
+    # create polynomialized version of the system (to solve with halleys)
     P2 = Model()
     P2.nx =3
     P2.x = ca.SX.sym("xw1w2",P2.nx)
     P2.model = ca.vertcat(P2.x[1]+P2.x[2],(-2)*P2.x[1]*(P2.x[1]+P2.x[2]), (-4)*P2.x[2]*(P2.x[1]+P2.x[2]))
 
 
+
+    # create polynomialized version of the system (to solve with newton)
+    P3 = Model()
+    P3.nx =3
+    P3.x = ca.SX.sym("xw1w2Newton",P3.nx)
+    P3.model = ca.vertcat(P3.x[1]+P3.x[2],(-2)*P3.x[1]*(P3.x[1]+P3.x[2]), (-4)*P3.x[2]*(P3.x[1]+P3.x[2]))
+
+
     # simulation hyper parameter
     timesimu = 100
-    numOfsteps =5
+    numOfsteps = 3
     dt = timesimu/numOfsteps
     # start Condition
     startCondition1 = ca.DM(1)
     startCondition2 = ca.DM([1,0.1353352832366127,0.01831563888873418]) # w1 = e^-2*1 = 0.13533... ;  w2 = e^-4*1 = 0.01831... ;
-    TestBench(P1,P2,numOfsteps, dt, startCondition1, startCondition2)
+    #TestBench(P1,P2,numOfsteps, dt, startCondition1, startCondition2)
 
-    #TestBench2(P1,P2,100,10,startCondition1,startCondition2)
+    TestBench2(P1,P2,P3,100,0.01,startCondition1,startCondition2)
     
